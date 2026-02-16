5d95983f9310c1f3e130d8facb966319dafe7498
EmmanuelJunior <142091756+EmmanuelJunuior@users.noreply.github.com>
2026-02-16 13:37:18 +0200

    Create stg_yellow_tripdata.sql

---
+++ models/staging/stg_yellow_tripdata.sql
@@ -0,0 +1,24 @@
+with source as (
+    select *
+    from `your_project_id.nytaxi.yellow_tripdata`
+)
+
+select
+    vendorid as vendor_id,
+    tpep_pickup_datetime as pickup_datetime,
+    tpep_dropoff_datetime as dropoff_datetime,
+    passenger_count,
+    trip_distance,
+    ratecodeid as rate_code_id,
+    store_and_fwd_flag,
+    pulocationid as pickup_location_id,
+    dolocationid as dropoff_location_id,
+    payment_type,
+    fare_amount,
+    extra,
+    mta_tax,
+    tip_amount,
+    tolls_amount,
+    improvement_surcharge,
+    total_amount
+from source
